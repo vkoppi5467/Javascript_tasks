2047453b02924f490eec0d767b7785f3b9be0f96
Vamsi krishna Koppineedi <vamsi.naidu1731@gmail.com>
2024-10-21 09:06:57 +0530

    Your commit message

--- arrays.js
+++ arrays.js
@@ -1,15 +1,19 @@
-const names = [];
-console.log(names);
-//Creating an Array and Initializing with Values
-const courses = ["HTML", "CSS", "Javascript", "React"];
-console.log(courses);
-//creating an array with empty value
-let name1 = new Array();
-console.log(name1);
-// Creating and Initializing an array with value
-let course1 = new Array("1","2", 1, 3);
-console.log(course1);
-//Initializing Array while declaring
-let name2 = new Array(1);
-name2[0] = 20;
-console.log(name2)
+// const names = [];
+// console.log(names);
+// //Creating an Array and Initializing with Values
+// const courses = ["HTML", "CSS", "Javascript", "React"];
+// console.log(courses);
+// //creating an array with empty value
+// let name1 = new Array();
+// console.log(name1);
+// // Creating and Initializing an array with value
+// let course1 = new Array("1","2", 1, 3);
+// console.log(course1);
+// //Initializing Array while declaring
+// let name2 = new Array(1);
+// name2[0] = 20;
+// console.log(name2)
+input = "1,2,3,4,5";
+array= input.split(",");
+numArray = array.map(Number);
+console.log(numArray);
